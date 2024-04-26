3310b907b3afef67d61d51341a6e8ebefc56287f
Erwann-Gallois <gallois.erwann@gmail.com>
2024-04-26 09:42:20 +0000

    Ajout des insertions dans la table famille_et pour les familles ATCOPIA32B, VANDAL11 et ATLINE1_1

--- insert_donnee_db.sql
+++ insert_donnee_db.sql
@@ -1,2 +1,22 @@
-INSERT INTO chromosome (num_k, taille, is_somatique) VALUES (1, 30973317, TRUE);
-INSERT INTO chromosome (num_k, taille, is_somatique) VALUES (2, )
+INSERT INTO chromosome (num_k, taille, is_somatique) VALUES (1, 30973317, TRUE); #ATCOPIA32B
+INSERT INTO chromosome (num_k, taille, is_somatique) VALUES (2, , TRUE); #VANDAL11
+
+-- Famille
+INSERT INTO famille_et (nom_famille, type_famille, ordre)
+VALUES (
+    'ATCOPIA32B'
+    'LTR',
+    'COPIA'
+  );
+INSERT INTO famille_et (nom_famille, type_famille, ordre)
+VALUES (
+    'VANDAL11'
+    'DNA',
+    'MuDR'
+  );
+INSERT INTO famille_et (nom_famille, type_famille, ordre)
+VALUES (
+    'ATLINE1_1'
+    'non-LTR',
+    'LINE-L1'
+  );
